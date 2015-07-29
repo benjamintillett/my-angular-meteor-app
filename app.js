c083c0fe59d84a8b44b20d1024a826f9d7013309
benjamin tillett <benjamin.tillett@gmail.com>
2015-07-29 08:46:30 +0100

    complete step three

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
+Parties = new Mongo.Collection("parties");
+
 if (Meteor.isClient){
 
 
-	function PartiesListCtrl(){
+	function PartiesListCtrl($meteor){
 		var self = this;
 
-		self.parties = [
-            {'name': 'Dubstep-Free Zone',
-              'description': 'Can we please just for an evening not listen to dubstep.'},
-            {'name': 'All dubstep all the time',
-              'description': 'Get it on!'},
-            {'name': 'Savage lounging',
-              'description': 'Leisure suit required. And only fiercest manners.'}
-        ];
+		self.parties = $meteor.collection(Parties);
+
+
 	}
 
 
 	angular.module('socially',['angular-meteor'])
 		.controller('PartiesListCtrl',PartiesListCtrl);
+}
+
+
+if (Meteor.isServer){
+	Meteor.startup(function(){
+		if (Parties.find().count() === 0){
+			var parties = [
+				{'name': 'Dubstep-Free Zone',
+				  'description': 'Can we please just for an evening not listen to dubstep.'},
+				{'name': 'All dubstep all the time',
+				  'description': 'Get it on!'},
+				{'name': 'Savage lounging',
+				  'description': 'Leisure suit required. And only fiercest manners.'}
+			];
+			for (var i = 0; i < parties.length; i++)
+				Parties.insert(parties[i]);
+		};
+	})
 }
